c060b01f866dac84f22f358b967f463e703e0e67
Mahamed-Ali <Mohamed.lumia540@outlook.com>
2025-02-22 23:33:56 +0200

    Added Laravel WebSecService project

---
+++ WebSecService/resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.master')
+@section('title', 'welcome')
+@section('content')
+    <div class="card m-4">
+        <div class="card-body">
+            Welcome to Home Page
+        </div>
+    </div>
+</body>
+    
+@endsection
---
+++ WebSecService/routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/multable/{number?}', function ($number=10) {
+    $j = $number;
+    return view('multable',compact("j")); 
+});
+
+Route::get('/even', function () {
+    return view('even'); 
+});
+
+Route::get('/prime', function () {
+    return view('prime'); 
+});
+   
